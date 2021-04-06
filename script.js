1e0fa8b5685766f8e5b16f25887d7f1eacdee10e
Shea Cronin <shealcronin@gmail.com>
2021-04-06 11:27:15 -0400

    Add function to toggle active styles on input focus/blur

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
-console.log('ello');
-
-class FormItem {
-    constructor(el) {
-        this.el = el;
-        this.el.addEventListener('input', (e) => this.checkValidity(e));
-    }
-    checkValidity(event) {
-        if (!this.el.validity.valid) {
-            console.log('There is an error.');
-            event.preventDefault();
-        }
-    }
+function toggleClass(el, cls) {
+    // Decide if complete class needs to be removed or added.
+    const action = el.classList.contains(cls) ? 'remove' : 'add';
+    // Remove/add the class.
+    el.classList[action](cls);
 }
 
-const email = new FormItem(document.querySelector('#email'));
+class FormItem {
+    constructor(container) {
+        this.container = container;
+        this.input = container.querySelector('input');
+        this.input.addEventListener('input', () => this.checkValidity());
+        this.input.addEventListener('focus', () =>
+            toggleClass(this.container, 'active')
+        );
+        this.input.addEventListener('blur', () =>
+            toggleClass(this.container, 'active')
+        );
+        this.error = container.querySelector('.error');
+    }
+    checkValidity() {
+        if (!this.input.validity.valid) {
+            console.log('There is an error.');
+        }
+    }
+    showError() {}
+}
 
-console.log(email);
+// const email = new FormItem(document.querySelector('#email'));
+
+const form = document.querySelector('form');
+const formItems = form.querySelectorAll('.form-item');
+formItems.forEach((item) => {
+    const formItem = new FormItem(item);
+    console.log(formItem);
+});
+// form.addEventListener('submit', )
+
+//console.log(email);
