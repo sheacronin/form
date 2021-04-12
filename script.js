12b18b2ce27329c4b5156df9e4ad57b9070f2bd7
Shea Cronin <shealcronin@gmail.com>
2021-04-12 15:04:39 -0400

    Add addError method to FormItem class that pushes error objects for what to check for, along with a message, into an array on each FormItem

--- script.js
+++ script.js
@@ -16,36 +16,79 @@ class FormItem {
             toggleClass(this.container, 'active')
         );
         this.container = input.parentElement;
-        this.error = this.container.querySelector('.error');
+        this.errors = [];
+        this.errorMessage = this.container.querySelector('.error');
+    }
+    addError(check, message, isCustom) {
+        const error = { check, message, isCustom };
+        this.errors.push(error);
     }
     checkValidity() {
-        if (this.input.validity.valid) {
-            this.error.textContent = '';
-            return true;
-        } else {
-            this.showError();
-            return false;
+        console.log('checking validity...');
+        for (let i = 0; i < this.errors.length; i++) {
+            // Store default false check.
+            let checkFailed = false;
+            if (this.errors[i].isCustom) {
+                console.log(this.errors[i].check);
+                // If error is custom, evaluate the check as it is.
+                checkFailed = this.errors[i].check();
+            } else {
+                // If the error's not custom, evaluate the check under the input's validity.
+                checkFailed = this.input.validity[this.errors[i].check];
+            }
+            // If the check is true, there is an error.
+            if (checkFailed) {
+                this.errorMessage.textContent = this.errors[i].message;
+                return false;
+            }
         }
-    }
-    showError() {
-        if (this.input.validity.valueMissing) {
-            this.error.textContent = 'Please enter an input.';
-        } else if (this.input.validity.typeMismatch) {
-            this.error.textContent = 'Entered value must be (type).';
-        } else if (this.input.validity.patternMismatch) {
-            this.error.textContent = 'Please enter a valid (type).';
-        }
+        // If looped through everything without finding error, then:
+        this.errorMessage.textContent = '';
+        return true;
     }
 }
 
 const form = document.querySelector('form');
 
-//const email = new FormItem(document.querySelector('#email'));
-
 const formItems = [...form.querySelectorAll('input')].map(
     (input) => new FormItem(input)
 );
 
+formItems.forEach((item) =>
+    item.addError('valueMissing', 'Please enter a value')
+);
+
+const email = formItems[0];
+email.addError(
+    'typeMismatch',
+    'Please enter a valid email address. Example: name@example.com'
+);
+
+const country = formItems[1];
+country.addError(
+    'patternMismatch',
+    'Please enter a valid country name, including only letters and spaces.'
+);
+country.addError(
+    'tooShort',
+    'Please enter a country name with at least 4 characters.'
+);
+const zipcode = formItems[2];
+zipcode.addError(
+    'patternMismatch',
+    'Please enter a valid zip code consisting of 5 digits or 5 digits, followed by a dash and 4 digits. Example: 12345 or 12345-1234.'
+);
+const password = formItems[3];
+const confirmPassword = formItems[4];
+confirmPassword.addError(
+    function () {
+        return password.input.value !== confirmPassword.input.value;
+    },
+    'Please enter the same password entered in the "Password" field.',
+    true
+);
+
+// Check each form item's validity before submitting form.
 form.addEventListener('submit', (e) => {
     for (let i = 0; i < formItems.length; i++) {
         const validity = formItems[i].checkValidity();
